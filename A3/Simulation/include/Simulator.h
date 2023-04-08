ee7c30da2358d945f0e2c1dafc6ca30c1bf77c7a
upuneetu <upuneetu@gmail.com>
2023-04-08 11:06:32 -0400

    ported from assignment 2

---
+++ A3/Simulation/include/Simulator.h
@@ -0,0 +1,40 @@
+//
+// Created by Anshuman Funkwal on 3/13/23.
+//
+
+#pragma once
+
+#include "../../Algorithm/MyAlgorithm.h"
+#include "../../Common/AbstractAlgorithm.h"
+#include "BatteryMeterImpl.h"
+#include "DirtSensorImpl.h"
+#include "RobotState.h"
+#include "Utils.h"
+#include "WallsSensorImpl.h"
+
+#include <fstream>
+#include <iostream>
+
+class Simulator {
+private:
+  std::size_t max_steps_;
+  std::size_t steps_ = 0;
+  std::string final_state_ = "";
+  House house_;
+  RobotState robot_state_;
+  std::vector<char> step_list_;
+  AbstractAlgorithm *algo;
+  // sensors
+  DirtSensorImpl dirt_sensor_;
+  WallsSensorImpl wall_sensor_;
+  BatteryMeterImpl battery_meter_;
+
+  int initSensors();
+
+public:
+  Simulator();
+  int readHouseFile(const std::string &house_file_path);
+  void setAlgorithm(AbstractAlgorithm &algorithm);
+  void run();
+  void dump(std::string outFileName);
+};
