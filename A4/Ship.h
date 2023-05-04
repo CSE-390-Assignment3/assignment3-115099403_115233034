47034eea115a469516635762ce4c4bb83bdd9d35
upuneetu <upuneetu@gmail.com>
2023-05-03 20:38:00 -0400

    renamed variables & added todos

--- A4/Ship.h
+++ A4/Ship.h
@@ -10,7 +10,7 @@
 #include <unordered_map>
 #include <vector>
 
-namespace exams {
+namespace shipping {
 template <typename T> class NamedType {
   T t;
 
@@ -27,84 +27,89 @@ struct Y : NamedType<int> {
   using NamedType<int>::NamedType;
 };
 
-using Position = std::tuple<exams::X, exams::Y>;
-} // namespace exams
+struct Height : NamedType<int> {
+  using NamedType<int>::NamedType;
+};
 
-// a short pause from exams to define hash for Position
+using Position = std::tuple<shipping::X, shipping::Y>;
+} // namespace shipping
+
+// a short pause from shipping to define hash for Position
 namespace std {
-template <> struct hash<exams::Position> {
-  std::size_t operator()(const exams::Position &pos) const noexcept {
+template <> struct hash<shipping::Position> {
+  std::size_t operator()(const shipping::Position &pos) const noexcept {
     return std::get<0>(pos) ^ (std::get<1>(pos) << 1);
   }
 };
 } // namespace std
 
-// back to the exams business
-namespace exams {
-class BadPositionException {
-  X x;
-  Y y;
+// back to the shipping business
+namespace shipping {
+// TODO: verify exception definition
+class BadShipOperationException {
+  // X x;
+  // Y y;
   std::string msg;
 
 public:
-  BadPositionException(X x, Y y, std::string msg)
-      : x(x), y(y), msg(std::move(msg)) {}
-  void print() const {
-    std::cout << msg << " : X {" << x << "}, Y {" << y << "}\n";
-  }
+  BadShipOperationException(X x, Y y, std::string msg) : msg(std::move(msg)) {}
+  void print() const { std::cout << msg << std::endl; }
 };
 
-template <typename Examinee>
+template <typename Container>
 using Grouping =
     std::unordered_map<std::string,
-                       std::function<std::string(const Examinee &)>>;
+                       std::function<std::string(const Container &)>>;
 
-template <typename Examinee> class ExamHall {
+template <typename Container> class Ship {
   class GroupView {
-    const std::unordered_map<Position, const Examinee &> *p_group = nullptr;
+    const std::unordered_map<Position, const Container &> *p_group = nullptr;
     using iterator_type =
-        typename std::unordered_map<Position, const Examinee &>::const_iterator;
+        typename std::unordered_map<Position,
+                                    const Container &>::const_iterator;
 
   public:
-    GroupView(const std::unordered_map<Position, const Examinee &> &group)
+    GroupView(const std::unordered_map<Position, const Container &> &group)
         : p_group(&group) {}
     GroupView(int) {}
     auto begin() const { return p_group ? p_group->begin() : iterator_type{}; }
     auto end() const { return p_group ? p_group->end() : iterator_type{}; }
   };
   class iterator {
-    using ExamineesItr =
-        typename std::vector<std::optional<Examinee>>::const_iterator;
-    ExamineesItr examinees_itr;
-    ExamineesItr examinees_end;
-    void set_itr_to_occupied_sit() {
-      while (examinees_itr != examinees_end && !(*examinees_itr)) {
-        ++examinees_itr;
+    using ContainersItr =
+        typename std::vector<std::optional<Container>>::const_iterator;
+    ContainersItr containers_itr;
+    ContainersItr containers_end;
+    void set_itr_to_occupied_load() {
+      while (containers_itr != containers_end && !(*containers_itr)) {
+        ++containers_itr;
       }
     }
 
   public:
-    iterator(ExamineesItr examinees_itr, ExamineesItr examinees_end)
-        : examinees_itr(examinees_itr), examinees_end(examinees_end) {
-      set_itr_to_occupied_sit();
+    iterator(ContainersItr containers_itr, ContainersItr containers_end)
+        : containers_itr(containers_itr), containers_end(containers_end) {
+      set_itr_to_occupied_load();
     }
     iterator operator++() {
-      ++examinees_itr;
-      set_itr_to_occupied_sit();
+      ++containers_itr;
+      set_itr_to_occupied_load();
       return *this;
     }
-    const Examinee &operator*() const { return examinees_itr->value(); }
+    const Container &operator*() const { return containers_itr->value(); }
     bool operator!=(iterator other) const {
-      return examinees_itr != other.examinees_itr;
+      return containers_itr != other.containers_itr;
     }
   };
 
-  std::vector<std::optional<Examinee>> examinees;
+  std::vector<std::optional<Container>> containers;
   int x_size;
   int y_size;
-  Grouping<Examinee> groupingFunctions;
-  using Pos2Examinee = std::unordered_map<Position, const Examinee &>;
-  using Group = std::unordered_map<std::string, Pos2Examinee>;
+  int h_size;
+
+  Grouping<Container> groupingFunctions;
+  using Pos2Container = std::unordered_map<Position, const Container &>;
+  using Group = std::unordered_map<std::string, Pos2Container>;
   // all groupings by their grouping name
   mutable std::unordered_map<std::string, Group> groups;
   // private method
@@ -112,62 +117,81 @@ template <typename Examinee> class ExamHall {
     if (x >= 0 && x < x_size && y >= 0 && y < y_size) {
       return y * x_size + x;
     }
-    throw BadPositionException(x, y, "index out of range");
+    throw BadShipOperationException(x, y, "index out of range");
   }
-  Examinee &get_examinee(X x, Y y) {
-    return examinees[pos_index(x, y)].value();
+  Container &get_container(X x, Y y) {
+    return containers[pos_index(x, y)].value();
   }
-  void addExamineeToGroups(X x, Y y) {
-    Examinee &e = get_examinee(x, y);
+  void addContainerToGroups(X x, Y y) {
+    Container &e = get_container(x, y);
     for (auto &group_pair : groupingFunctions) {
       groups[group_pair.first][group_pair.second(e)].insert(
           {std::tuple{x, y}, e});
     }
   }
-  void removeExamineeFromGroups(X x, Y y) {
-    Examinee &e = get_examinee(x, y);
+  void removeContainerFromGroups(X x, Y y) {
+    Container &e = get_container(x, y);
     for (auto &group_pair : groupingFunctions) {
       groups[group_pair.first][group_pair.second(e)].erase(std::tuple{x, y});
     }
   }
 
 public:
-  ExamHall(X x, Y y, Grouping<Examinee> groupingFunctions) noexcept
-      : examinees(x * y), x_size(x), y_size(y),
+  // TODO: create containers for x*y*h in ctors
+  // TODO: implement restrictions
+
+  Ship(X x, Y y, Height max_height) noexcept
+      : x_size(x), y_size(y), h_size(max_height) {}
+
+  Ship(X x, Y y, Height max_height,
+       std::vector<std::tuple<X, Y, Height>> restrictions) noexcept(false)
+      : : x_size(x), y_size(y), h_size(max_height) {}
+
+  Ship(X x, Y y, Height max_height,
+       std::vector<std::tuple<X, Y, Height>> restrictions,
+       Grouping<Container> groupingFunctions) noexcept(false)
+      : x_size(x), y_size(y), h_size(max_height),
         groupingFunctions(std::move(groupingFunctions)) {}
 
-  void sit(X x, Y y, Examinee e) noexcept(false) {
-    auto &seat = examinees[pos_index(x, y)];
-    if (seat) {
-      throw BadPositionException(x, y, "occupied sit");
+  Ship(X x, Y y, Grouping<Container> groupingFunctions) noexcept
+      : containers(x * y), x_size(x), y_size(y),
+        groupingFunctions(std::move(groupingFunctions)) {}
+
+  void load(X x, Y y, Container c) noexcept(false) {
+    // TODO: handle height of the container
+    auto &compartment = containers[pos_index(x, y)];
+    if (compartment) {
+      throw BadShipOperationException(x, y, "occupied load");
     }
-    seat = std::move(e);
-    addExamineeToGroups(x, y);
+    compartment = std::move(c);
+    addContainerToGroups(x, y);
   }
 
-  Examinee unsit(X x, Y y) noexcept(false) {
-    auto &seat = examinees[pos_index(x, y)];
-    if (!seat) {
-      throw BadPositionException(x, y, "no examinee to unsit");
+  Container unload(X x, Y y) noexcept(false) {
+    auto &compartment = containers[pos_index(x, y)];
+    if (!compartment) {
+      throw BadShipOperationException(x, y, "no container to unload");
     }
-    removeExamineeFromGroups(x, y);
-    auto examinee = std::optional<Examinee>{}; // empty seat
-    // swap empty seat with real examinee and return the real examinee
+    removeContainerFromGroups(x, y);
+    auto container = std::optional<Container>{}; // empty compartment
+    // swap empty compartment with real container and return the real container
     // might be more efficient than creating a temp copy
-    std::swap(seat, examinee); // seat would become empty, examinee would get
-                               // the real examinee
-    return examinee.value();
+    std::swap(compartment, container); // compartment would become empty,
+                                       // container would get the real container
+    return container.value();
   }
   void move(X from_x, Y from_y, X to_x, Y to_y) noexcept(false) {
     // note that this implementation is problematic - if to "to" location is bad
-    // an excpetion would be thrown after examinee already unsitted from
-    // original seat leaving us in an invalidated state
-    // TODO: fix above issue
-    sit(to_x, to_y, unsit(from_x, from_y));
+    // an excpetion would be thrown after container already unloaded from
+    // original compartment leaving us in an invalidated state
+    // TODO: fix above issue (from skeleton)
+    // TODO: add height checking & throw exception
+    load(to_x, to_y, unload(from_x, from_y));
   }
 
-  GroupView getExamineesViewByGroup(const std::string &groupingName,
-                                    const std::string &groupName) const {
+  // TODO: verify if this works out of box from ExamHall
+  GroupView getContainersViewByGroup(const std::string &groupingName,
+                                     const std::string &groupName) const {
     auto itr = groups.find(groupingName);
     if (itr == groups.end() &&
         groupingFunctions.find(groupingName) != groupingFunctions.end()) {
@@ -187,20 +211,22 @@ public:
       auto itr2 = grouping.find(groupName);
       if (itr2 == grouping.end()) {
         std::tie(itr2, std::ignore) =
-            itr->second.insert({groupName, Pos2Examinee{}});
+            itr->second.insert({groupName, Pos2Container{}});
         //--------------------------------------------------------------------
         // OR, with auto tuple unpack
         //--------------------------------------------------------------------
         // auto [insert_itr, _] = itr->second.insert({groupName,
-        // Pos2Examinee{}}); itr2 = insert_itr;
+        // Pos2Container{}}); itr2 = insert_itr;
       }
       return GroupView{itr2->second};
     }
     return GroupView{0};
   }
+  // TODO: implement API
+  GroupView getContainersViewByPosition(X x, Y y) const { return GroupView{0}; }
 
-  iterator begin() const { return {examinees.begin(), examinees.end()}; }
-  iterator end() const { return {examinees.end(), examinees.end()}; }
+  iterator begin() const { return {containers.begin(), containers.end()}; }
+  iterator end() const { return {containers.end(), containers.end()}; }
 
   //-------------------------------------------------------
   // Note:
@@ -215,10 +241,10 @@ public:
   // see also - spec open proposal:
   // http://www.open-std.org/jtc1/sc22/wg21/docs/lwg-active.html#2321
   //-------------------------------------------------------
-  ExamHall(const ExamHall &) = delete;
-  ExamHall &operator=(const ExamHall &) = delete;
-  ExamHall(ExamHall &&) = default;
-  ExamHall &operator=(ExamHall &&) = default;
+  Ship(const Ship &) = delete;
+  Ship &operator=(const Ship &) = delete;
+  Ship(Ship &&) = default;
+  Ship &operator=(Ship &&) = default;
   //-------------------------------------------------------
 };
-} // namespace exams
+} // namespace shipping
